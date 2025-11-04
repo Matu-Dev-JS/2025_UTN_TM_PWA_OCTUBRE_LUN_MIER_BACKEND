9a94f8eb4590e669e60016033d3ace255093534d
matu.dev <mati.a.gimenez2002@gmail.com>
2025-11-04 11:14:44 -0300

    Clase 25

--- src/controllers/messages.controller.js
+++ src/controllers/messages.controller.js
@@ -1,8 +1,18 @@
+import MessageService from "../services/messages.service"
+
 class MessagesController {
     static async getAllByChannelId(request, response){
         try{    
             const {channel_selected, member} = request
-
+            const {messages} = await MessageService.getAllByChannelId(channel_selected._id)
+            response.status(200).json({
+                ok: true,
+                status: 200,
+                message: "Messages",
+                data: {
+                    messages: messages
+                },
+            });
         }
         catch(error){
 
@@ -16,6 +26,17 @@ class MessagesController {
             const {content} = request.body
             //Crear un mensaje
             //(Para la clase que viene) Obtener la lista de mensajes y responder
+            const {messages, message_created} = await MessageService.create(content, member._id, channel_selected._id)
+
+            response.status(201).json({
+                ok: true,
+                status: 201,
+                message: "Messages created",
+                data: {
+                    messages: messages,
+                    message_created: message_created
+                },
+            });
         }
         catch(error){
 
